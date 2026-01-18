df947828a922ead1f849cf24ba01774ab8c96568
Pasa <pavlo1191@gmail.com>
2026-01-18 19:20:44 +0100

    refactor: code formatting and cleanup

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,11 +1,9 @@
 function Logo() {
   return (
-    <>
-      <div className="flex flex-col text-white">
-        <span className="text-3xl">WEATHER</span>
-        <span className="text-2xl text-white/60">FORECAST</span>
-      </div>
-    </>
+    <div className="flex flex-col text-white">
+      <span className="text-3xl">WEATHER</span>
+      <span className="text-2xl text-white/60">FORECAST</span>
+    </div>
   );
 }
 
