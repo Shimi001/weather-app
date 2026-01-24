4e5d58c04c85b21901a5f9dd22f50c8266e61077
Pasa <pavlo1191@gmail.com>
2026-01-24 03:02:16 +0100

    feat: add day symbols and date utility

--- src/data/days.ts
+++ src/data/days.ts
@@ -1,9 +1,9 @@
 export const days = [
-  { name: "Monday", color: "bg-red-400" },
-  { name: "Tuesday", color: "bg-yellow-400" },
-  { name: "Wednesday", color: "bg-emerald-400" },
-  { name: "Thursday", color: "bg-blue-400" },
-  { name: "Friday", color: "bg-violet-400" },
-  { name: "Saturday", color: "bg-fuchsia-400" },
-  { name: "Sunday", color: "bg-rose-400" },
+  { name: "Sunday", symbol: "日", color: "bg-rose-400" },
+  { name: "Monday", symbol: "月", color: "bg-red-400" },
+  { name: "Tuesday", symbol: "火", color: "bg-yellow-400" },
+  { name: "Wednesday", symbol: "水", color: "bg-emerald-400" },
+  { name: "Thursday", symbol: "木", color: "bg-blue-400" },
+  { name: "Friday", symbol: "金", color: "bg-violet-400" },
+  { name: "Saturday", symbol: "土", color: "bg-fuchsia-400" },
 ];
