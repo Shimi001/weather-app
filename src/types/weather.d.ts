1a7060231164c386b285bba6fe05af9a8be87717
Pasa <pavlo1191@gmail.com>
2026-01-25 16:16:45 +0100

    feat: extend weather API types with forecast data

--- src/types/weather.d.ts
+++ src/types/weather.d.ts
@@ -1,3 +1,21 @@
+export interface DayWeather {
+  maxtemp_c: number;
+  mintemp_c: number;
+  avgtemp_c: number;
+  maxwind_kph: number;
+  daily_chance_of_rain: number;
+  daily_chance_of_snow: number;
+  condition: {
+    text: string;
+    icon: string;
+  };
+}
+
+export interface ForecastDay {
+  date: string;
+  day: DayWeather;
+}
+
 export default interface WeatherData {
   location: {
     name: string;
@@ -8,5 +26,8 @@ export default interface WeatherData {
       text: string;
       icon: string;
     };
+  };
+  forecast: {
+    forecastday: ForecastDay[];
   };
 }
