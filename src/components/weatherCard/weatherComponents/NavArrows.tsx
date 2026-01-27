7be8422e99480e891b7a1409ca67437831a736f5
Pasa <pavlo1191@gmail.com>
2026-01-27 20:40:10 +0100

    refactor: improve arrow button disable logic

--- src/components/weatherCard/weatherComponents/NavArrows.tsx
+++ src/components/weatherCard/weatherComponents/NavArrows.tsx
@@ -1,13 +1,14 @@
 import { useWeatherStore } from "../../../store/weatherStore";
 
 function NavArrows() {
-  const { incrementDay, decrementDay } = useWeatherStore();
+  const { incrementDay, decrementDay, dayOffset } = useWeatherStore();
 
   return (
     <>
       {/* left button */}
       <button
         onClick={decrementDay}
+        disabled={dayOffset === 0}
         className="absolute left-0 top-1/4 -translate-x-11 text-white/70 text-8xl"
       >
         ‹
@@ -15,6 +16,7 @@ function NavArrows() {
       {/* right button */}
       <button
         onClick={incrementDay}
+        disabled={dayOffset === 2}
         className="absolute right-0 top-1/4 translate-x-11 text-white/70 text-8xl"
       >
         ›
